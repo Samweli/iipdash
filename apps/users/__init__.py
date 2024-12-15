b468f5e97924e4e0e63f31742c562baf21e8db52
lykmapipo <lallyelias87@gmail.com>
2024-12-16 01:16:35 +0300

    docs(users): fix package level documentation

---
+++ apps/users/__init__.py
@@ -0,0 +1,11 @@
+"""
+Custom User application.
+
+This package contains the custom user model and its associated configurations,
+admin interface, and related utilities for authentication and user management
+in a Django project.
+
+References:
+    - :class:`django.contrib.auth.models.AbstractUser`
+    - :class:`django.apps.AppConfig`
+"""
