d0ba8aee854be7da700c895ec01f335e18c1ad80
lykmapipo <lallyelias87@gmail.com>
2025-01-14 19:04:15 +0300

    feat(education): improve institution ownerships API serializer
    This also:
    - addresses #33
    - add education institution ownerships openapi examples

--- apps/education/api/openapi/examples/ownership.py
+++ apps/education/api/openapi/examples/ownership.py
@@ -1,18 +1,18 @@
 """
-OpenAPI Examples for :class:`education.api.views.CategoryViewSet`.
+OpenAPI Examples for :class:`education.api.views.OwnershipViewSet`.
 
 This module defines reusable OpenAPI examples for documenting the responses of
-educational institution categories API endpoints using `drf-spectacular`.
+educational institution ownerships API endpoints using `drf-spectacular`.
 
 Attributes:
-    category_response_only (Dict[str, Any]):
+    ownership_response_only (Dict[str, Any]):
         A detailed representation of a single educational institution
-        category in JSON format.
+        ownership in JSON format.
 
-    category_list_response_only (OpenApiExample):
+    ownership_list_response_only (OpenApiExample):
         An OpenAPI example for the list endpoint response in JSON paginated object list format.
 
-    category_retrieve_response_only (OpenApiExample):
+    ownership_retrieve_response_only (OpenApiExample):
         An OpenAPI example for the retrieve endpoint response in JSON single
         object format.
 
@@ -24,32 +24,32 @@ from typing import Any, Dict
 
 from drf_spectacular.utils import OpenApiExample
 
-#: A detailed JSON representation of a single educational institution category.
-category_response_only: Dict[str, Any] = {
+#: A detailed JSON representation of a single educational institution ownership.
+ownership_response_only: Dict[str, Any] = {
     "uuid": "550e8400-e29b-41d4-a716-446655440000",
-    "name": "university",
-    "code": "university",
-    "description": "An educational institution offering higher education.",
+    "name": "public",
+    "code": "public",
+    "description": "An educational institution operated by local, state, or national governments.",
     "created_at": "2025-01-01T12:00:00Z",
     "updated_at": "2025-01-02T12:00:00Z",
     "extras": {"type": "academic"},
 }
 
 #: OpenAPI example for the list endpoint response (paginated object list).
-category_list_response_only: OpenApiExample = OpenApiExample(
+ownership_list_response_only: OpenApiExample = OpenApiExample(
     name="Successful Response",
     value={
         "count": 123,
-        "next": "/api/education/catergories/?page=3",
-        "previous": "/api/education/catergories/?page=1",
-        "results": [category_response_only],
+        "next": "/api/education/ownerships/?page=3",
+        "previous": "/api/education/ownerships/?page=1",
+        "results": [ownership_response_only],
     },
     response_only=True,
 )
 
 #: OpenAPI example for the retrieve endpoint response (single object).
-category_retrieve_response_only: OpenApiExample = OpenApiExample(
+ownership_retrieve_response_only: OpenApiExample = OpenApiExample(
     name="Successful Response",
-    value=category_response_only,
+    value=ownership_response_only,
     response_only=True,
 )
--- apps/education/api/serializers.py
+++ apps/education/api/serializers.py
@@ -1,6 +1,5 @@
 from typing import List, Type
 
-from drf_spectacular.utils import OpenApiExample, extend_schema_serializer
 from rest_framework import serializers
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 
@@ -65,24 +64,8 @@ class CategorySerializer(serializers.ModelSerializer):
         read_only_fields: List[str] = ["id", "uuid", "created_at", "updated_at"]
 
 
-@extend_schema_serializer(
-    examples=[
-        OpenApiExample(
-            name="Successful Response",
-            value={
-                "uuid": "550e8400-e29b-41d4-a716-446655440000",
-                "name": "Public",
-                "code": "public",
-                "description": "An institutions operated by local, state, or national governments.",
-                "created_at": "2025-01-01T12:00:00Z",
-                "updated_at": "2025-01-02T12:00:00Z",
-                "extras": {"type": "academic"},
-            },
-            response_only=True,
-        )
-    ]
-)
 class OwnershipSerializer(serializers.ModelSerializer):
+    """Ownership type of an educational institution."""
 
     class Meta:
         """Metadata for the :class:`OwnershipSerializer`.
@@ -92,10 +75,10 @@ class OwnershipSerializer(serializers.ModelSerializer):
                 The model class that this serializer represents.
 
             fields (List[str]):
-                The list of fields to be included in the serialized data.
+                A list of fields to be included in the serialized data.
 
             read_only_fields (List[str]):
-                Fields that cannot be modified during updates.
+                A list of fields that cannot be modified during updates.
         """
 
         model: Type[Ownership] = Ownership
