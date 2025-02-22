58caa30d64727da084ea82af8e5548d127be4819
lykmapipo <lallyelias87@gmail.com>
2025-02-23 01:35:54 +0300

    feat(ui): add basic home UI layout
    This:
    - addresses #88
    - adds map main layout
    - adds home basic sidebar
    - adds basic home choose layer tab layout
    - adds basic view presets tab layout

--- assets/js/education_dash.js
+++ assets/js/education_dash.js
@@ -1,87 +1,86 @@
 import * as Vue from 'vue';
-import axios from 'axios';
+import axios from 'axios'; // eslint-disable-line no-unused-vars
 import maplibregl from 'maplibre-gl';
 
-import * as settings from './conf';
+import * as settings from './conf'; // eslint-disable-line no-unused-vars
 import * as maps from './maps';
 
-
 const EducationDash = {
-    components: {},
+  components: {},
 
-    data () {
-        return {
-            _map: null,
-            _mapID: '_education-map',
-            lookup: {},
-            results: {features: []}
-        }
+  data() {
+    return {
+      _map: null,
+      _mapID: '_education-map',
+      lookup: {},
+      results: { features: [] },
+    };
+  },
+
+  methods: {
+    initLookup() {},
+
+    updateData: async function () {},
+
+    initMap() {
+      const map = new maplibregl.Map({
+        ...maps.basemap,
+        container: '_education-map',
+      });
+
+      const navigationControl = new maplibregl.NavigationControl({
+        visualizePitch: true,
+        visualizeRoll: true,
+        showZoom: true,
+        showCompass: true,
+      });
+      map.addControl(navigationControl);
+
+      this._map = Vue.markRaw(map);
     },
 
-    methods: {
+    updateMap: async function () {
+      this._map.on('load', () => {
+        // country boundaries
+        this._map.addSource('countries', maps.sources.countries);
+        this._map.addLayer(maps.layers.countries);
 
-        initLookup () {},
+        // education summary statistics by area
+        this._map.addSource('areas-education', maps.sources['areas-education']);
+        this._map.addLayer(maps.layers['areas-education']);
 
-        updateData: async function () {},
+        // education institutions
+        this._map.addSource(
+          'education-institutions',
+          maps.sources['education-institutions'],
+        );
+        this._map.addLayer(maps.layers['education-institutions']);
 
-        initMap () {
-            const map = new maplibregl.Map({
-                ...maps.basemap,
-                container: '_education-map'
-            });
-
-            const navigationControl = new maplibregl.NavigationControl({
-                visualizePitch: true,
-                visualizeRoll: true,
-                showZoom: true,
-                showCompass: true
-            });
-            map.addControl(navigationControl);
-
-            this._map = Vue.markRaw(map);
-
-        },
-
-        updateMap: async function () {
-            this._map.on('load', () => {
-
-                // country boundaries
-                this._map.addSource('countries', maps.sources['countries']);
-                this._map.addLayer(maps.layers['countries']);
-
-                // education summary statistics by area
-                this._map.addSource('areas-education', maps.sources['areas-education']);
-                this._map.addLayer(maps.layers['areas-education']);
-
-                // education institutions
-                this._map.addSource('education-institutions', maps.sources['education-institutions']);
-                this._map.addLayer(maps.layers['education-institutions']);
-
-                // fiber nodes
-                this._map.addSource('fiber-nodes', maps.sources['fiber-nodes']);
-                this._map.addLayer(maps.layers['fiber-nodes']);
-            });
-       },
-
-        update: async function () {
-            try {
-                await this.updateData();
-                this.updateMap();
-            } catch (e) {
-                console.log(e)
-            }
-
-            const event = new Event('page-updated');
-            window.dispatchEvent(event);
-        },
+        // fiber nodes
+        this._map.addSource('fiber-nodes', maps.sources['fiber-nodes']);
+        this._map.addLayer(maps.layers['fiber-nodes']);
+      });
     },
 
-    mounted () {
-        this.initLookup();
-        this.initMap();
-        this.update();
-    }
-}
+    update: async function () {
+      try {
+        await this.updateData();
+        this.updateMap();
+      } catch (e) {
+        console.log(e);
+      }
+
+      const event = new Event('page-updated');
+      window.dispatchEvent(event);
+    },
+  },
+
+  mounted() {
+    this.initLookup();
+    this.initMap();
+    this.update();
+  },
+};
 
 const app = Vue.createApp(EducationDash);
 app.mount('#_education-dash');
