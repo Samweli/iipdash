dc3dc554e783b7ced7754f08596aa8db53dc37bb
machakux <bmachaku@gmail.com>
2025-03-27 00:15:52 +0300

    fix: remove duplicate url

--- iipdash/urls.py
+++ iipdash/urls.py
@@ -63,7 +63,6 @@ from .api_urls import router as api_router
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
-    path("", include("dashboards.urls")),
     path("about/", TemplateView.as_view(template_name="about/about.html"), name="about"),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
