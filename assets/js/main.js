60e2957f3755a032075a9e5d10156733b874d1c9
machakux <bmachaku@gmail.com>
2025-03-28 11:23:56 +0300

    fix(ui): re-include bootstrap js in main.js
    resolves #114

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,2 @@
 // import * as $ from 'jquery'; // eslint-disable-line no-unused-vars
-// import * as bootstrap from 'bootstrap'; // eslint-disable-line no-unused-vars
+import * as bootstrap from 'bootstrap'; // eslint-disable-line no-unused-vars
