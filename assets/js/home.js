748fa2eeb391aed7ee08f74b2821c71c2d058505
lykmapipo <lallyelias87@gmail.com>
2025-04-05 11:05:04 +0300

    feat(ui): fix toggling `population-density-hd` data layer in home ui

--- assets/js/home.js
+++ assets/js/home.js
@@ -323,7 +323,15 @@ const HomeDash = {
 
             // 2.1 There are `catalogSelectedLayers`, show only selected catalog layers
             if (this.catalogSelectedLayers && this.catalogSelectedLayers.length >= 1) {
-                // 2.1.1 Toggle `education-institutions` layer visibility
+                // 2.1.1 Toggle `population-density-hd` layer visibility
+                const showPopulationDensityHDLayer = this.catalogSelectedLayers.includes('population-density-hd');
+                if (showPopulationDensityHDLayer) {
+                    this._map.setLayoutProperty('population-density-hd', 'visibility', 'visible');
+                } else {
+                    this._map.setLayoutProperty('population-density-hd', 'visibility', 'none');
+                }
+
+                // 2.1.2 Toggle `education-institutions` layer visibility
                 const showEducationInstitutionsLayer = this.catalogSelectedLayers.includes('education-institutions');
                 if (showEducationInstitutionsLayer) {
                     this._map.setLayoutProperty('education-institutions', 'visibility', 'visible');
@@ -331,7 +339,7 @@ const HomeDash = {
                     this._map.setLayoutProperty('education-institutions', 'visibility', 'none');
                 }
 
-                // 2.1.2 Toggle `fiber-nodes` layer visibility
+                // 2.1.3 Toggle `fiber-nodes` layer visibility
                 const showFiberNodesLayer = this.catalogSelectedLayers.includes('fiber-nodes');
                 if (showFiberNodesLayer) {
                     this._map.setLayoutProperty('fiber-nodes', 'visibility', 'visible');
@@ -339,7 +347,7 @@ const HomeDash = {
                     this._map.setLayoutProperty('fiber-nodes', 'visibility', 'none');
                 }
 
-                // 2.1.3 Toggle `mobile-coverage-3g` layer visibility
+                // 2.1.4 Toggle `mobile-coverage-3g` layer visibility
                 const showMobileCoverage3GLayer = this.catalogSelectedLayers.includes('mobile-coverage-3g');
                 if (showMobileCoverage3GLayer) {
                     this._map.setLayoutProperty('mobile-coverage-3g', 'visibility', 'visible');
@@ -347,7 +355,7 @@ const HomeDash = {
                     this._map.setLayoutProperty('mobile-coverage-3g', 'visibility', 'none');
                 }
 
-                // 2.1.4 Toggle `mobile-coverage-4g` layer visibility
+                // 2.1.5 Toggle `mobile-coverage-4g` layer visibility
                 const showMobileCoverage4GLayer = this.catalogSelectedLayers.includes('mobile-coverage-4g');
                 if (showMobileCoverage4GLayer) {
                     this._map.setLayoutProperty('mobile-coverage-4g', 'visibility', 'visible');
