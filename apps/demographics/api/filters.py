6466ed7d72994aab9909c5e867bb229f713654b6
lykmapipo <lallyelias87@gmail.com>
2025-06-14 07:22:46 +0300

    feat(demographics): add relative wealth index API filter
    - add relative wealth index filter in `RelativeWealthIndexViewSet`
    - implement relative wealth index filterset
    - improve apidocs and docstrings for `RelativeWealthIndexViewSet`
    - add csv export to `RelativeWealthIndexViewSet`
    - implement relative wealth index csv serializer

--- apps/demographics/api/filters.py
+++ apps/demographics/api/filters.py
@@ -1,10 +1,12 @@
+from typing import List, Type
+
 from django.utils.translation import gettext_lazy as _
 
 from django_filters import rest_framework as filters
 
-from ..models import PopulationDensityHD
+from ..models import PopulationDensityHD, RelativeWealthIndex
 
-__all__ = ["PopulationDensityHDFilter"]
+__all__ = ["PopulationDensityHDFilter", "RelativeWealthIndexFilter"]
 
 
 class PopulationDensityHDFilter(filters.FilterSet):
@@ -55,4 +57,128 @@ class PopulationDensityHDFilter(filters.FilterSet):
             "administrative_area_in",
             "population_density_gte",
             "population_density_lte",
+        ]
+
+
+class RelativeWealthIndexFilter(filters.FilterSet):
+    """
+    FilterSet for the :class:`demographics.models.RelativeWealthIndex` model.
+
+    This filter allows filtering `RelativeWealthIndex` objects based on specific
+    fields.
+
+    Examples:
+        Filter by country code (exact match):
+            `/api/demographics/relative-wealth-index/?country=MW`
+
+        Filter by multiple country codes (in comparison):
+            `/api/demographics/relative-wealth-index/?country_in=MW,ZM`
+
+        Filter by administrative area UUID (exact match):
+            `/api/demographics/relative-wealth-index/?administrative_area=04bcbe53-
+            98da-4ff5-96a8-d626c6da45cc`
+
+        Filter by multiple administrative area UUIDs (in comparison):
+            `/api/demographics/relative-wealth-index/?administrative_area_in=04bcbe53-
+            98da-4ff5-96a8-d626c6da45cc,032ffe37-67d6-40d4-a65d-144f12bbd493`
+
+        Filter by relative wealth index (greater than or equal):
+            `/api/demographics/relative-wealth-index/?rwi_gte=5216`
+
+        Filter by relative wealth index (less than or equal):
+            `/api/demographics/relative-wealth-index/?rwi_lte=5216`
+
+    Attributes:
+        country (:class:`django_filters.rest_framework.filters.CharFilter`):
+            A filter for matching the `administrative_area__country` field
+            of the `RelativeWealthIndex` model using `exact, case-insensitive` comparison.
+
+        country_in (:class:`django_filters.rest_framework.filters.BaseInFilter`):
+            A filter for matching multiple `administrative_area__country` field
+            of the `RelativeWealthIndex` model using `in` comparison.
+
+        administrative_area (:class:`django_filters.rest_framework.filters.UUIDFilter`):
+            A filter for matching the `administrative_area__uuid` field of
+            the `RelativeWealthIndex` model using `uuid equal` comparison.
+
+        administrative_area_in (:class:`django_filters.rest_framework.filters.BaseInFilter`):
+            A filter for matching multiple `administrative_area__uuid` field of
+            the `RelativeWealthIndex` model using `in` comparison.
+
+        rwi_gte (:class:`django_filters.rest_framework.filters.NumberFilter`):
+            A filter for matching the `rwi` field of the `RelativeWealthIndex`
+            model using `gte` comparison.
+
+        rwi_lte (:class:`django_filters.rest_framework.filters.NumberFilter`):
+            A filter for matching the `rwi` field of the `RelativeWealthIndex`
+            model using `lte` comparison.
+    """
+
+    #: Filter by country code (exact match, case-insensitive)
+    country: filters.CharFilter = filters.CharFilter(
+        field_name="administrative_area__country",
+        lookup_expr="iexact",
+        help_text=_("Filter by country code."),
+    )
+
+    #: Filter by multiple country codes
+    country_in: filters.BaseInFilter = filters.BaseInFilter(
+        field_name="administrative_area__country",
+        help_text=_("Filter by multiple country codes."),
+    )
+
+    #: Filter by administrative area UUID (exact match)
+    administrative_area: filters.UUIDFilter = filters.UUIDFilter(
+        field_name="administrative_area__uuid",
+        help_text=_("Filter by administrative area UUID."),
+    )
+
+    #: Filter by multiple administrative area UUIDs
+    administrative_area_in: filters.BaseInFilter = filters.BaseInFilter(
+        field_name="administrative_area__uuid",
+        help_text=_("Filter by multiple administrative area UUIDs."),
+    )
+
+    #: Filter by administrative area level (exact match)
+    administrative_area_level: filters.NumberFilter = filters.NumberFilter(
+        field_name="administrative_area__depth",
+        help_text=_("Filter by administrative area level."),
+    )
+
+    #: Filter by relative wealth index (greater than or equal)
+    rwi_gte: filters.NumberFilter = filters.NumberFilter(
+        field_name="rwi",
+        lookup_expr="gte",
+        help_text=_("Filter by relative wealth index greater than or equal i.e `>=`."),
+    )
+
+    #: Filter by relative wealth index (less than or equal)
+    rwi_lte: filters.NumberFilter = filters.NumberFilter(
+        field_name="rwi",
+        lookup_expr="lte",
+        help_text=_("Filter by relative wealth index less than or equal i.e `<=`."),
+    )
+
+    class Meta:
+        """
+        Metadata for the :class:`RelativeWealthIndexFilter`.
+
+        Attributes:
+            model (Type[RelativeWealthIndex]):
+                A Django model associated with this filter.
+                In this case, it's the :class:`RelativeWealthIndex` model.
+
+            fields (List[str]):
+                A list of field names (i.e query parameters) available for filtering.
+        """
+
+        model: Type[RelativeWealthIndex] = RelativeWealthIndex
+        fields: List[str] = [
+            "country",
+            "country_in",
+            "administrative_area",
+            "administrative_area_in",
+            "administrative_area_level",
+            "rwi_gte",
+            "rwi_lte",
         ]
--- apps/demographics/api/views.py
+++ apps/demographics/api/views.py
@@ -1,6 +1,8 @@
+from typing import List, Type
+
 from django.conf import settings
 from django.contrib.gis.db.models import PointField
-from django.db.models import F
+from django.db.models import F, QuerySet
 from django.db.models.functions import Cast
 from django.utils.decorators import method_decorator
 from django.utils.translation import gettext_lazy as _
@@ -9,16 +11,19 @@ from django.views.decorators.cache import cache_page
 from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework.decorators import action
-from rest_framework.filters import OrderingFilter
+from rest_framework.filters import BaseFilterBackend, OrderingFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework_gis.pagination import GeoJsonPagination
 from vectortiles.backends.postgis import VectorLayer
 from vectortiles.rest_framework.renderers import MVTRenderer
 
+from core.api.filters import DistanceToPointFilter, InBBoxFilter, TMSTileFilter
+from core.api.mixins import CSVDownloadMixin
+
 from ..models import PopulationDensityHD, RelativeWealthIndex
-from .filters import PopulationDensityHDFilter
-from .serializers import PopulationDensityHDSerializer, RelativeWealthIndexSerializer
+from .filters import PopulationDensityHDFilter, RelativeWealthIndexFilter
+from .serializers import PopulationDensityHDSerializer, RelativeWealthIndexCSVSerializer, RelativeWealthIndexSerializer
 
 MVT_CACHE_ALIAS = settings.CACHE_MVT_ALIAS
 MVT_CACHE_TIMEOUT = settings.CACHE_TIMEOUTS["mvt"]
@@ -96,17 +101,88 @@ class PopulationDensityHDViewSet(VectorLayer, ReadOnlyModelViewSet):
         return Response(self.get_tile(x=int(kwargs.get("x")), y=int(kwargs.get("y")), z=int(kwargs.get("z"))))
 
 
-class RelativeWealthIndexViewSet(VectorLayer, ReadOnlyModelViewSet):
-    """Relative wealth index API endpoint."""
+@extend_schema_view(
+    list=extend_schema(
+        summary=_("Relative Wealth Indexes"),
+        description=_(
+            "Retrieve a list of relative wealth indexes, with optional searching, filtering, ordering and pagination."
+        ),
+    ),
+    retrieve=extend_schema(
+        summary=_("Relative Wealth Index"),
+        description=_("Retrieve details of a specific relative wealth index."),
+    ),
+    tile=extend_schema(summary=_("Relative Wealth Indexes Vector Tiles")),
+    download=extend_schema(summary=_("Relative Wealth Indexes CSV")),
+)
+class RelativeWealthIndexViewSet(CSVDownloadMixin, VectorLayer, ReadOnlyModelViewSet):
+    """
+    A ViewSet for managing :class:`demographics.models.RelativeWealthIndex` objects.
 
-    serializer_class = RelativeWealthIndexSerializer
-    lookup_field = "uuid"
-    required_scopes = ["default"]
-    pagination_class = GeoJsonPagination
+    This viewset provides endpoints for:
 
-    filter_backends = [OrderingFilter]
-    ordering_fields = ["created_at", "updated_at"]
-    ordering = ["-created_at"]
+    - Listing all relative wealth indexes (`list` endpoint).
+    - Retrieving a specific relative wealth index by UUID (`retrieve` endpoint).
+    - Export relative wealth indexes to a `CSV` file (`download` endpoint).
+    - Provides Mapbox Vector Tiles (`mvt`) for relative wealth indexes (`tiles` endpoint).
+    """
+
+    #: A serializer class for converting `RelativeWealthIndex` objects to GeoJSON format.
+    serializer_class: Type[RelativeWealthIndexSerializer] = RelativeWealthIndexSerializer
+
+    #: A serializer class for converting `RelativeWealthIndex` objects to CSV format.
+    csv_serializer_class: Type[RelativeWealthIndexCSVSerializer] = RelativeWealthIndexCSVSerializer
+
+    #: A pagination class to handle GeoJSON format pagination for `RelativeWealthIndex` objects.
+    pagination_class: Type[GeoJsonPagination] = GeoJsonPagination
+
+    #: A lookup field used to retrieve a specific `RelativeWealthIndex` object.
+    lookup_field: str = "uuid"
+
+    #: A list of required OAuth2 scopes for accessing the `RelativeWealthIndex` API endpoints.
+    required_scopes: List[str] = ["default"]
+
+    #: A list of filter backends for applying search and order filters
+    #: to the queryset of `RelativeWealthIndex` objects.
+    filter_backends: List[Type[BaseFilterBackend]] = [
+        DjangoFilterBackend,
+        OrderingFilter,
+        InBBoxFilter,
+        TMSTileFilter,
+        DistanceToPointFilter,
+    ]
+
+    #: A custom filter for applying complex filters to the queryset of
+    #: `RelativeWealthIndex` objects.
+    filterset_class: Type[RelativeWealthIndexFilter] = RelativeWealthIndexFilter
+
+    #: A list of fields that are used for ordering the queryset of
+    #: `RelativeWealthIndex` objects (e.g., `?ordering=<field>`).
+    ordering_fields: List[str] = ["rwi", "error", "created_at", "updated_at"]
+
+    #: A `RelativeWealthIndex` geometry field used in performing bounding box filtering
+    #: on the queryset of `RelativeWealthIndex` objects via query parameters
+    #: (i.e., `?in_bbox=<bbox>`).
+    bbox_filter_field: str = "geometry"
+
+    #: Whether to include `RelativeWealthIndex` objects that overlap the bounding box
+    #: on the queryset.
+    bbox_filter_include_overlapping: bool = False
+
+    #: A `RelativeWealthIndex` geometry field used in filtering queryset of `RelativeWealthIndex`
+    #: objects based on their distance from a specific point via query
+    #: parameters (i.e., `?point=<x,y>&radius=<distance>`).
+    distance_filter_field: str = "geometry"
+
+    #: Whether to convert the distance value to meters before filtering
+    #: queryset of `RelativeWealthIndex` objects based on their distance from a
+    #: specific point
+    distance_filter_convert_meters: bool = True
+
+    #: A default queryset for retrieving `RelativeWealthIndex` objects.
+    queryset: QuerySet[RelativeWealthIndex] = (
+        RelativeWealthIndex.objects.select_related("administrative_area").order_by("-created_at").all()
+    )
 
     #: Vector tiles layer ID
     id = "relative-wealth-index"
@@ -115,25 +191,44 @@ class RelativeWealthIndexViewSet(VectorLayer, ReadOnlyModelViewSet):
     tile_fields = (
         "rwi",
         "error",
+        "country",
+        "administrative_area_uuid",
+        "administrative_area_name",
     )
     tile_buffer = 64
 
-    queryset = RelativeWealthIndex.objects.all()
-
     def get_vector_tile_queryset(self, *args, **kwargs):
         """Returns a queryset used to generate vector tiles."""
-        queryset = self.get_queryset().annotate(geom=Cast("geometry", PointField()))
+        queryset = self.get_queryset().annotate(
+            geom=Cast("geometry", PointField()),
+            country=F("administrative_area__country"),
+            administrative_area_uuid=F("administrative_area__uuid"),
+            administrative_area_name=F("administrative_area__name"),
+        )
         queryset = self.filter_queryset(queryset)
         return queryset
 
     @action(
         detail=False,
         methods=["get"],
+        name="Download Relative Wealth Indexes CSV",
+        url_path="download",
+        url_name="list-download",
+    )
+    def download(self, request, *args, **kwargs):
+        """Download relative wealth indexes as CSV file."""
+
+        return self.export_csv(request, *args, **kwargs)
+
+    @action(
+        detail=False,
+        methods=["get"],
+        name="Relative Wealth Indexes Vector Tiles",
         renderer_classes=(MVTRenderer,),
         url_path=r"tiles/(?P<z>\d+)/(?P<x>\d+)/(?P<y>\d+).mvt",
         url_name="tile",
     )
     @method_decorator(cache_page(MVT_CACHE_TIMEOUT, key_prefix="mvt:relative-wealth-index", cache=MVT_CACHE_ALIAS))
     def tile(self, request, *args, **kwargs):
-        """Provides Mapbox Vector Tiles for relative wealth index."""
+        """Provides Mapbox Vector Tiles for relative wealth indexes."""
         return Response(self.get_tile(x=int(kwargs.get("x")), y=int(kwargs.get("y")), z=int(kwargs.get("z"))))
