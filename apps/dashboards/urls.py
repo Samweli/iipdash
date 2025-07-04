b9f409d233f4cc739838919211f7c3b72602d2a2
machakux <bmachaku@gmail.com>
2025-07-04 19:28:57 +0300

    chore: clean up

--- apps/dashboards/urls.py
+++ apps/dashboards/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = "dashboards"
 
 urlpatterns = [
-    # path("", views.HomeView.as_view(), name="home"),
     path("education/", views.EducationView.as_view(), name="education"),
     path("infrastructure", views.InfrastructureView.as_view(), name="infrastructure"),
     path("hazard", views.HazardView.as_view(), name="hazard"),
