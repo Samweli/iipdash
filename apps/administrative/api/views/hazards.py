0a6e81490947cf67868a3c85823c71c91df49993
Samweli <smwltwesa6@gmail.com>
2025-07-10 14:21:21 +0300

    feat(hazard-api): fix wrong population expose summation from hazard exposure

--- apps/administrative/api/views/hazards.py
+++ apps/administrative/api/views/hazards.py
@@ -1,11 +1,14 @@
 from django.contrib.postgres.aggregates import ArrayAgg
-from django.db.models import Q, Sum
+from django.db.models import IntegerField, OuterRef, Subquery, Sum, Value
+from django.db.models.functions import Coalesce
 from django.utils.timezone import now
 from django.utils.translation import gettext_lazy as _
 
 from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework.decorators import action
+
+from hazards.models import HazardExposure
 
 from ...models import Area
 from ..filters import AreaHazardExposureFilter
@@ -36,22 +39,72 @@ class AreaHazardExposureViewSet(AreaViewSet):
     filterset_class = AreaHazardExposureFilter
 
     def get_queryset(self):
+        """
+        Returns a queryset of administrative areas annotated with hazard exposure data.
+
+        This method:
+
+        - Filter first the queryset using the request parameters
+          via the `filterset_class` to ensure that only relevant data is considered before any annotation or aggregation.
+
+        - The `population_exposed` and `population_exposed_ev`
+          annotations use `Subquery` to perform accurate aggregation of hazard exposure values per administrative area.
+          This avoids inflated results due to duplicate joins across related models.
+
+        - Applies `.distinct()` to the queryset to avoid duplicate administrative areas in the
+         API response when multiple hazard exposures exist.
+
+        Returns:
+            QuerySet: A queryset of administrative areas with annotated hazard exposure data.
+        """
         qs = super().get_queryset()
 
-        filterset = self.filterset_class(self.request.GET, queryset=qs)
+        query_params = self.request.GET.copy()
+        if "hazard_type" in query_params:
+            query_params.setlist("hazards__code", query_params.getlist("hazard_type"))
+
+        if "urbanization_degree" in query_params:
+            query_params.setlist("urbanization_degree__code", query_params.getlist("urbanization_degree"))
+
+        filterset = self.filterset_class(query_params, queryset=qs)
         qs = filterset.qs
 
-        qs = qs.annotate(
-            population_exposed=Sum("hazards_exposure_coverage__exposure__population_exposed", distinct=True),
-            population_exposed_ev=Sum("hazards_exposure_coverage__exposure__population_exposed_ev", distinct=True),
-            urbanization_degree_codes=ArrayAgg(
-                "hazards_exposure_coverage__exposure__urbanization_degree__code",
-                distinct=True,
-            ),
-            hazard_codes=ArrayAgg(
-                "hazards_exposure_coverage__exposure__hazards__code",
-                distinct=True,
-            ),
+        hazard_codes = query_params.getlist("hazards__code")
+        urban_codes = query_params.getlist("urbanization_degree__code")
+
+        hazard_qs = HazardExposure.objects.filter(coverage__administrative_area=OuterRef("pk"))
+        hazard_ev_qs = HazardExposure.objects.filter(coverage__administrative_area=OuterRef("pk"))
+
+        if hazard_codes:
+            hazard_qs = hazard_qs.filter(hazards__code__in=hazard_codes)
+            hazard_ev_qs = hazard_ev_qs.filter(hazards__code__in=hazard_codes)
+        if urban_codes:
+            hazard_qs = hazard_qs.filter(urbanization_degree__code__in=urban_codes)
+            hazard_ev_qs = hazard_ev_qs.filter(urbanization_degree__code__in=urban_codes)
+
+        hazard_qs = (
+            hazard_qs.values("coverage__administrative_area")
+            .annotate(total_exposed=Sum("population_exposed"))
+            .values("total_exposed")[:1]
+        )
+
+        hazard_ev_qs = (
+            hazard_ev_qs.values("coverage__administrative_area")
+            .annotate(total_exposed_ev=Sum("population_exposed_ev"))
+            .values("total_exposed_ev")[:1]
+        )
+
+        qs = (
+            qs.annotate(
+                population_exposed=Coalesce(Subquery(hazard_qs, output_field=IntegerField()), Value(0)),
+                population_exposed_ev=Coalesce(Subquery(hazard_ev_qs, output_field=IntegerField()), Value(0)),
+                urbanization_degree_codes=ArrayAgg(
+                    "hazards_exposure_coverage__exposure__urbanization_degree__code", distinct=True
+                ),
+                hazard_codes=ArrayAgg("hazards_exposure_coverage__exposure__hazards__code", distinct=True),
+            )
+            .order_by("id", "name")
+            .distinct()
         )
 
         return qs
